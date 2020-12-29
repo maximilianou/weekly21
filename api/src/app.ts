c8d42278fc67398cf706358304f6a934b5ea3539
Maximiliano Usich <maximilianou@gmail.com>
2020-12-29 14:37:04 -0300

    feat(api): start http hello

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,1 +1,11 @@
-console.log('Running.. TypeScript app')
+import {createServer} from './utils/server';
+
+createServer()
+  .then( server => {
+      server.listen( 3021, () => {
+          console.info(`Listening on port: ${3021}`);
+      })
+  })
+  .catch( err => {
+      console.error(`Error:: ${err}`);
+  });
